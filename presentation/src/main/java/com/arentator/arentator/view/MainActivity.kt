783c469c7003611022664658f1cc01eb9a6cea38
darthoo <darthoo12@gmail.com>
2018-02-21 22:14:06 +0300

    Added profile screen (stub)

--- presentation/src/main/java/com/arentator/arentator/view/MainActivity.kt
+++ presentation/src/main/java/com/arentator/arentator/view/MainActivity.kt
@@ -3,14 +3,24 @@ package com.arentator.arentator.view
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.view.View
 
 import com.arentator.arentator.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this,RegisterActivity::class.java))
+        card.setOnClickListener{startProfileActivity()}
+    }
+
+    private fun startProfileActivity(){
+        val avatarTransition = resources.getString(R.string.user_avatar_transition)
+        val intent = Intent(this, ProfileActivity::class.java)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, avatar as View, avatarTransition)
+        startActivity(intent, options.toBundle())
     }
 }
---
+++ presentation/src/main/java/com/arentator/arentator/view/ProfileActivity.kt
@@ -0,0 +1,22 @@
+package com.arentator.arentator.view
+
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.transition.Explode
+import com.arentator.arentator.R
+import kotlinx.android.synthetic.main.view_profile.*
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            photoHeader.translationZ = 6f
+            photoHeader.invalidate()
+        }
+
+    }
+}
