6114fdbbb122c3ea13cbf5c6372ad526b5da616b
darthoo <darthoo12@gmail.com>
2018-02-24 23:11:10 +0300

    Added first impl of circular reveal

--- presentation/src/main/java/com/arentator/arentator/view/MainActivity.kt
+++ presentation/src/main/java/com/arentator/arentator/view/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityOptionsCompat
+import android.transition.Transition
 import android.view.View
 
 import com.arentator.arentator.R
@@ -15,12 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         card.setOnClickListener{startProfileActivity()}
+
+        window.sharedElementEnterTransition.addListener(object : Transition.TransitionListener{
+            override fun onTransitionEnd(p0: Transition?) {
+                avatar.animate().alpha(1f)
+                roundedImage.animate().alpha(0f)
+            }
+            override fun onTransitionResume(p0: Transition?) {}
+            override fun onTransitionPause(p0: Transition?) {}
+            override fun onTransitionCancel(p0: Transition?) {}
+            override fun onTransitionStart(p0: Transition?) {}
+
+        })
     }
 
     private fun startProfileActivity(){
-        val avatarTransition = resources.getString(R.string.user_avatar_transition)
+        avatar.animate().alpha(0f)
+        roundedImage.animate().alpha(1f)
+        val avatarTransition = resources.getString(R.string.rounded_color_view_transition)
         val intent = Intent(this, ProfileActivity::class.java)
-        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, avatar as View, avatarTransition)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, roundedImage as View, avatarTransition)
         startActivity(intent, options.toBundle())
     }
 }
--- presentation/src/main/java/com/arentator/arentator/view/ProfileActivity.kt
+++ presentation/src/main/java/com/arentator/arentator/view/ProfileActivity.kt
@@ -4,8 +4,16 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.transition.Explode
+import android.transition.TransitionInflater
 import com.arentator.arentator.R
 import kotlinx.android.synthetic.main.view_profile.*
+import android.opengl.ETC1.getWidth
+import android.transition.ChangeClipBounds
+import android.transition.Transition
+
+import android.view.View
+import android.view.ViewAnimationUtils
+
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -13,10 +21,33 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            userAvatar.translationZ = 6f
-            userAvatar.invalidate()
-        }
+        window.sharedElementEnterTransition.addListener(object :Transition.TransitionListener{
+            override fun onTransitionEnd(p0: Transition?) {
+                reveal()
+            }
+            override fun onTransitionResume(p0: Transition?) {}
+            override fun onTransitionPause(p0: Transition?) {}
+            override fun onTransitionCancel(p0: Transition?) {}
+            override fun onTransitionStart(p0: Transition?) {}
 
+        })
     }
+
+    fun reveal() {
+        val center = roundedImage.getCenter()
+        val anim = ViewAnimationUtils.createCircularReveal(toolbarImage, center.first.toInt(),
+                center.second.toInt(), 0f, toolbarImage.width.toFloat())
+                .apply {
+                    duration = 400
+                }
+        toolbarImage.visibility = View.VISIBLE
+        anim.start()
+    }
+
+    fun View.getCenter(): Pair<Float, Float> {
+        val cx = this.x + this.width / 2
+        val cy = this.y + this.height / 2
+        return Pair(cx, cy)
+    }
+
 }
